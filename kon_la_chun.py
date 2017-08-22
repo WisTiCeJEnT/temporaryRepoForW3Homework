4afdb5bf1d7b088343ddaa80fc934687a7695aaf
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-08-23 02:50:54 +0700

    kon la chun done

--- kon_la_chun.py
+++ kon_la_chun.py
@@ -1,7 +1,20 @@
+import math
 n = int(input())
-Ml = [0,0,0]
+inpl=[]
 ans = 0
+a = b = -1
 for i in range (0,n):
-    ans+=1
-    Ml[0] = int(input())
-    
+    inpl.append(int(input()))
+    if(inpl[len(inpl)-1]>8):
+        print("error")
+        exit()
+while(inpl):
+    ans += 1
+    a = inpl.pop(0)
+    if(inpl):
+        if(math.fabs(a-inpl[0]) <= 1):
+            b = inpl.pop(0)
+            if(inpl):
+                if(math.fabs(a-inpl[0])<=1 or math.fabs(b-inpl[0])<=1):
+                    inpl.pop(0)
+print(ans)
